7a60c71f3145158f7f90d4c725f4447b775e9e77
Mayank singh <mayanksingh0713@gmail.com>
2023-10-31 17:42:17 +0530

    second comit

--- server/sockets/websocketServer.js
+++ server/sockets/websocketServer.js
@@ -1,4 +1,10 @@
 const socketIO = require('socket.io');
+const express = require('express');
+const mongoose = require('mongoose');
+const Auction = require('../models/Auction');
+const Bid = require('../models/Bid');
+const Transaction = require('../models/Transaction');
+
 
 function setupWebSocketServer(httpServer) {
   const io = socketIO(httpServer, {
@@ -19,8 +25,89 @@ function setupWebSocketServer(httpServer) {
   
     socket.on('bid', (room, bidAmount) => {
       console.log('Bid amount:', bidAmount);
+      console.log('Room:', room);
+
+      if (bidAmount < 0) {
+        return;
+      }
+
+      if (bidAmount<= Auction.highestBid) {
+        return;
+      }
+
+      const newBid = new Bid({
+        amount: bidAmount,
+        auction: room,
+      });
+
+      newBid.save((err, bid) => {
+        if (err) {
+          console.log(err);
+        } else {
+          Auction.findById(room, (err, auction) => {
+            if (err) {
+              console.log(err);
+            } else {
+              auction.bids.push(bid);
+              auction.highestBid = bid;
+              auction.save((err, auction) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log('Auction updated');
+                }
+              });
+            }
+          });
+        }
+      }
+      );
+
       io.to(room).emit('newBid', bidAmount); // Broadcast the bid to all clients in the room
     });
+
+    socket.on('transaction', (room, buyer, seller) => {
+      console.log('Room:', room);
+      console.log('Buyer:', buyer);
+      console.log('Seller:', seller);
+
+      Auction.findById(room, (err, auction) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const newTransaction = new Transaction({
+            auction: auction,
+            buyer: buyer,
+            seller: seller,
+          });
+
+          newTransaction.save((err, transaction) => {
+            if (err) {
+              console.log(err);
+            } else {
+              auction.finalTransaction = transaction;
+              auction.save((err, auction) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log('Auction updated');
+                }
+              });
+            }
+          });
+        }
+      });
+
+      io.to(room).emit('newTransaction', buyer, seller); // Broadcast the transaction to all clients in the room
+    }
+    );
+
+    socket.on('leaveRoom', (room) => {
+      console.log('A user left room:', room);
+      socket.leave(room); // Leave the room
+    }
+    );
+
   
     // Handle disconnect event
     socket.on('disconnect', () => {
